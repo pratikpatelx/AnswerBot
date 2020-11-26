eb306425e9b632d5d643b37e2b536f63a57947af
swetul <33759252+swetul@users.noreply.github.com>
2020-11-26 00:03:36 -0600

    Add files via upload
    Setup file with working XML reader for Posts and Votes. fully working script, tested with 50k records each.

--- setup.py
+++ setup.py
@@ -35,14 +35,16 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
-def insert_data(conn):
+def insert_data(conn, fileName, tab):
     # Path should be a relative path from your pc
     # parser = etree.XMLParser(recover=true)
     # root = etree.XML('smallP.xml')
-    tree = ET.parse('smallP.xml')
+
+    # insert posts
+    tree = ET.parse(fileName)
     root = tree.getroot()
     curs = conn.cursor()
-    #root = ET.tostring(root, encoding='utf8').decode('utf8')
+    # root = ET.tostring(root, encoding='utf8').decode('utf8')
     print("root of the posts is ", root)
     count = 0
     insert_query = "INSERT INTO {table} ({columns}) VALUES ({values});"
@@ -52,25 +54,30 @@ def insert_data(conn):
         for i in range(len(child.attrib.values())-1):
             val = val + "?, "
 
-        print(len(child.attrib.keys()), "--------------",
-              len(child.attrib.values()))
         val = val + "?"
-        query = insert_query.format(table="posts", columns=', '.join(
+        query = insert_query.format(table=tab, columns=', '.join(
             child.attrib.keys()), values=val)
 
         curs.execute(query, child.attrib.values())
 
         conn.commit()
         count = count + 1
-        if count > 1000:
+
+        if(count > 1000):
             break
+
+    print("\n --------------------------------------------------------------------------------------------")
+    print("Done Inserting ", tab)
+    print("\n --------------------------------------------------------------------------------------------\n\n")
+
+    # inserting Votes to the DB
 
 
 def main():
     database = "pythonsqlite.db"
 
     sql_create_posts_table = """
-    CREATE TABLE IF NOT EXISTS posts (Id INTEGER PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS Posts (Id INTEGER PRIMARY KEY,
         PostTypeId INTEGER,
         ParentID INTEGER,
         AcceptedAnswerId INTEGER,
@@ -93,17 +100,32 @@ def main():
         CommunityOwnedDate DATETIME,
         ContentLicense TEXT
         );"""
+    sql_create_votes_table = """
+    CREATE TABLE IF NOT EXISTS Votes(
+        Id INTEGER,
+        PostId INTEGER,
+        UserId INTEGER,
+        VoteTypeId INTEGER,
+        CreationDate DATETIME,
+        BountyAmount INTEGER
+    );"""
 
     conn = create_connection(database)
 
     if conn is not None:
         # create projects table
         create_table(conn, sql_create_posts_table)
+        create_table(conn, sql_create_votes_table)
 
     else:
-        print("Error! cannot create the database connection.")
+        print("Error creating tables for DB")
 
-    insert_data(conn)
+    print("Please wait......")
+    insert_data(conn, 'smallP.xml', "Posts")
+    print("DONEPOSTS")
+    print("Please wait......")
+    insert_data(conn, 'smallV.xml', "Votes")
+    print("DONEVOTES")
 
     close_connection(conn)
 
